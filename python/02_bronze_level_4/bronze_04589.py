04c6f204e003b4f2e79d65dec66b7d2552038705
qoocrab <qoocrab@gmail.com>
2023-06-06 13:42:22 +0900

    feat: Python - 브론즈 4 - 3765, 4299, 4470, 4589, 4696

---
+++ python/02_bronze_level_4/bronze_04589.py
@@ -0,0 +1,9 @@
+print("Gnomes:")
+
+for _ in range(int(input())):
+    a, b, c = map(int, input().split())
+
+    if (a <= b and b <= c) or (a >= b and b >= c):
+        print("Ordered")
+    else:
+        print("Unordered")
